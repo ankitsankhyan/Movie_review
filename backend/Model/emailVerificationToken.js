cdeeb1c092c19b6a5a118dcd1e2d5d451380b430
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-06-11 19:17:53 +0530

    adding token schema

---
+++ backend/Model/emailVerificationToken.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const emailVerificationTokenSchema = new mongoose.Schema({
+
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    token:{
+        type: String,
+        required: true,
+
+    }
+    ,createdAt:{
+        type: Date,
+        // this means when to expire the token
+        expires: 3600,
+        default: Date.now()
+    }
+});
+// arrow function don't have this keyword and hence don't work here
+emailVerificationTokenSchema.pre("save", async function (next) {
+    if (this.isModified("token")) {
+// Note this 10 means how exintensive the hashing should be but it takes more time
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('EmailVerificationToken', emailVerificationTokenSchema);
