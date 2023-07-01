3d261f8191a60a7bc55a0494d3171dff11b0ab32
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-07-02 02:17:09 +0530

    added links

--- backend/routes/actor.js
+++ backend/routes/actor.js
@@ -1,9 +1,36 @@
+const express = require("express");
+const {
+  createActor,
+  updateActor,
+  removeActor,
+  searchActor,
+  getLatestActors,
+  getSingleActor,
+} = require("../controllers/actor");
+const { uploadImage } = require("../middlewares/multer");
+const { actorInfoValidator, validate } = require("../middlewares/validator");
 
-const express = require('express');
 const router = express.Router();
-const ActorController = require('../controller/actor');
-const {uploadImage} = require('../middleware/multer');
-const {actorValidator,validationHandler} = require('../middleware/validator');
-router.post('/create',uploadImage.single("avatar"),actorValidator,validationHandler, ActorController.create);
+
+router.post(
+  "/create",
+  uploadImage.single("avatar"),
+  actorInfoValidator,
+  validate,
+  createActor
+);
+
+router.post(
+  "/update/:actorId",
+  uploadImage.single("avatar"),
+  actorInfoValidator,
+  validate,
+  updateActor
+);
+
+router.delete("/:actorId", removeActor);
+router.get("/search", searchActor);
+router.get("/latest-uploads", getLatestActors);
+router.get("/single/:id", getSingleActor);
 
 module.exports = router;
