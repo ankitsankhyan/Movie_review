4e396d88ef211e902b4b9fe79978971267521b6e
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-07-02 02:12:33 +0530

    updating actor controller

--- backend/controller/actor.js
+++ backend/controller/actor.js
@@ -1,11 +1,7 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require('../cloud/index')
 const Actor = require("../Model/actor");
-cloudinary.config({ 
-  cloud_name: process.env.cloud_name, 
-  api_key: process.env.api_key, 
-  api_secret: process.env.api_secret,
-  secure: true
-});
+const {sendError, formatActor} = require('../utils/helper');
+
 // secure creates https url
 
 module.exports.create = async(req, res) => {
@@ -15,7 +11,7 @@ try{
   const actor = new Actor({
     name,about, gender
   });
- console.log(file);
+
   if (file) {
     const result = await cloudinary.uploader.upload(file.path, {pages:true});
    console.log(result);
@@ -27,17 +23,110 @@ try{
   }
   
   await actor.save();
-  res.send(
-    `<img src="${actor.avatar.url}" alt="">`
-  )
+  res.status(200).json({
+    message: "Actor created successfully",
+    actor
+  });
+
+   
 }catch(e){
   console.log(e);
   // throw new Error(e.message);
-  res.status(e.http_code).json({
-    message: e.message
-  });
+  sendError(res, e, e.error || 500);
 
 }
  
   
+};
+
+// update
+// Things to consider while updating.
+// No.1 - is image file is / avatar is also updating.
+// No.2 - if yes then remove old image before uploading new image / avatar.
+
+exports.updateActor = async (req, res) => {
+  const { name, about, gender } = req.body;
+  const { file } = req;
+  const { actorId } = req.params;
+
+  if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!");
+
+  const actor = await Actor.findById(actorId);
+  if (!actor) return sendError(res, "Invalid request, record not found!");
+
+  const public_id = actor.avatar?.public_id;
+
+  // remove old image if there was one!
+  if (public_id && file) {
+    const { result } = await cloudinary.uploader.destroy(public_id);
+    if (result !== "ok") {
+      return sendError(res, "Could not remove image from cloud!");
+    }
+  }
+
+  // upload new avatar if there is one!
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path
+    );
+    actor.avatar = { url: secure_url, public_id };
+  }
+
+  actor.name = name;
+  actor.about = about;
+  actor.gender = gender;
+
+  await actor.save();
+ 
+  res.status(201).json(formatActor(actor));
+};
+
+exports.removeActor = async (req, res) => {
+  const { actorId } = req.params;
+
+  if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!");
+
+  const actor = await Actor.findById(actorId);
+  if (!actor) return sendError(res, "Invalid request, record not found!");
+
+  const public_id = actor.avatar?.public_id;
+
+  // remove old image if there was one!
+  if (public_id) {
+    const { result } = await cloudinary.uploader.destroy(public_id);
+    if (result !== "ok") {
+      return sendError(res, "Could not remove image from cloud!");
+    }
+  }
+
+  await Actor.findByIdAndDelete(actorId);
+
+  res.json({ message: "Record removed successfully." });
+};
+
+exports.searchActor = async (req, res) => {
+  const { query } = req;
+  const result = await Actor.find({ $text: { $search: `"${query.name}"` } });
+
+  const actors = result.map((actor) => formatActor(actor));
+
+  res.json(actors);
+};
+
+exports.getLatestActors = async (req, res) => {
+  const result = await Actor.find().sort({ createdAt: "-1" }).limit(12);
+
+  const actors = result.map((actor) => formatActor(actor));
+
+  res.json(actors);
+};
+
+exports.getSingleActor = async (req, res) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) return sendError(res, "Invalid request!");
+
+  const actor = await Actor.findById(id);
+  if (!actor) return sendError(res, "Invalid request, actor not found!", 404);
+  res.json(formatActor(actor));
 };
--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,18 +1,39 @@
-const crypto = require('crypto')
+const crypto = require("crypto");
+const cloudinary = require("../cloud");
+exports.sendError = (res, error, statusCode = 401) =>
+  res.status(statusCode).json({ error });
 
-module.exports.sendError = (res, err, statusCode = 401)=>{
-    res.status(statusCode).json({err});
-}
+exports.generateRandomByte = () => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(30, (err, buff) => {
+      if (err) reject(err);
+      const buffString = buff.toString("hex");
 
+      resolve(buffString);
+    });
+  });
+};
 
-module.exports.generateRandomByte = ()=>{
-    return new Promise((resolve, reject)=>{
-        crypto.randomBytes(30,(err, buff)=>{
-            if(err) reject(err);
-            const buffString = buff.toString("hex");
-            resolve(buffString);
-        })
-    })
-}
+exports.handleNotFound = (req, res) => {
+  this.sendError(res, "Not found", 404);
+};
 
+exports.uploadImageToCloud = async (file) => {
+  const { secure_url: url, public_id } = await cloudinary.uploader.upload(
+    file,
+    { gravity: "face", height: 500, width: 500, crop: "thumb" }
+  );
 
+  return { url, public_id };
+};
+
+exports.formatActor = (actor) => {
+  const { name, gender, about, _id, avatar } = actor;
+  return {
+    id: _id,
+    name,
+    about,
+    gender,
+    avatar: avatar?.url,
+  };
+};
