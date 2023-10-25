e7b36f35cfd1ce214f033ccef7e058856712cd92
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-10-25 15:32:24 +0530

    resolved dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,38 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signin from "./components/auth/Signin";
+import Signup from "./components/auth/Signup";
+import EmailVerification from "./components/auth/EmailVerification";
+import Home from "./components/Home";
+import Navbar from "./components/user/Navbar";
+import ForgetPassword from "./components/auth/ForgetPassword";
+import ConfirmPassword from "./components/auth/ConfirmPassword";
+import NotFound from "./components/NotFound";
+import { useAuth } from "./hooks";
+import AdminNavigator from "./navigator/AdminNavigator";
+import SingleMovie from "./components/user/SingleMovie";
+import MovieReviews from "./components/user/MovieReviews";
 
-import './App.css';
+export default function App() {
+  const { authInfo } = useAuth();
+  const isAdmin = authInfo.profile?.role === "admin";
 
-import Signin from './components/auth/signin';
-import Signup from './components/auth/signup';
-import { Route,Routes } from 'react-router-dom';
-import Home from './components/user/Home';
-import EmailVerification from './components/auth/EmailVerification';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ConfirmPassword from './components/auth/ConfirmPassword';
-function App() {
+  if (isAdmin) return <AdminNavigator />;
+
   return (
-  <>
-
-  <Routes>
-    <Route path="/" element={<Home/>}>
-    <Route path="/auth/signin" element={<Signin/>}/>
-    <Route path="/auth/signup" element={<Signup/>}/>
-    <Route path="/auth/verification" element={<EmailVerification/>}/>
-    <Route path="/auth/forgot-password" element={<ForgotPassword/>}/>
-    <Route path="/auth/reset-password" element={<ConfirmPassword/>}/>
-    <Route path="*" element={<h1>404 Not Found</h1>}/>
-    </Route>
-    
-  </Routes>
-  
-  </>
-  
-
- 
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/signin" element={<Signin />} />
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/verification" element={<EmailVerification />} />
+        <Route path="/auth/forget-password" element={<ForgetPassword />} />
+        <Route path="/auth/reset-password" element={<ConfirmPassword />} />
+        <Route path="/movie/:movieId" element={<SingleMovie />} />
+        <Route path="/movie/reviews/:movieId" element={<MovieReviews />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/main.css';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import "./index.css";
+import ContextProviders from "./context";
 
-import Contextprovider from './context/contextproviders';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-  
-   
-    <BrowserRouter>
-    <Contextprovider>
-    <App />
-    </Contextprovider>
-    </BrowserRouter>
-   
-   
-  </React.StrictMode>
+  <BrowserRouter>
+    <ContextProviders>
+      <App />
+    </ContextProviders>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
